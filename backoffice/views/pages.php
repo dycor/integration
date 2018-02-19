bd12110b2ee8cf744baa9fd69fce8af4ec56b36c
chams95 <hicham95260@gmail.com>
2018-02-19 17:22:24 +0100

    Integration des pages

---
+++ backoffice/views/pages.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pages</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../../css/pages.css">
+	<link rel="stylesheet" type="text/css" href="../../css/grille.css">
+	<link rel="stylesheet" type="text/css" href="../../css/ionicons.css">
+</head>
+<body>
+<main>
+	
+	<div class="container-fluid">
+		<!-- Pages -->
+		<div class="row">
+			<div class="col-sm-12">
+				<section id="pages">
+					<div class="row">
+						<div class="col-12 relative">
+							<h2 id="page-title">PAGES</h2>
+							<div class="filtre">
+								<select>
+									<option>Toutes</option>
+								</select>
+							</div>
+							
+						</div>
+					</div>
+					<div class="row">
+						<ul class="offset-1 col-10 offset-1 list">
+							<li>
+								<div class="list-title">FORFAITS</div>
+								<div class="list-details">Presentation et tarifs des forfaits proposés...</div>
+								<div class="panel">
+									<a href="#"><i class="ion-ios-compose-outline edit"></i></a>
+									<a href="#"><i class="ion-ios-trash-outline delete"></i></a>
+								</div>
+							</li>
+							<li>
+								<div class="list-title">COMMENTAIRES</div>
+								<div class="list-details">Commentaires laissés par les élèves...</div>
+								<div class="panel">
+
+									<a href="#"><i class="ion-ios-compose-outline edit"></i></a>
+									<a href="#"><i class="ion-ios-trash-outline delete"></i></a>
+								</div>
+							</li>
+							<li>
+								<div class="list-title">DESCRIPTION</div>
+								<div class="list-details">Description de l'entreprise et des services...</div>
+								<div class="panel">
+									<a href="#"><i class="ion-ios-compose-outline edit"></i></a>
+									<a href="#"><i class="ion-ios-trash-outline delete"></i></a>
+								</div>
+							</li>
+							<li>
+								<div class="list-title">CONTACT</div>
+								<div class="list-details">Formulaire de contact et informations</div>
+								<div class="panel">
+									<a href="#"><i class="ion-ios-compose-outline edit"></i></a>
+									<a href="#"><i class="ion-ios-trash-outline delete"></i></a>
+								</div>
+							</li>
+							<li>
+								<div class="list-title">NOUVEAU...</div>
+								<div class="list-details">Crée une page</div>
+								<div class="panel">
+									<a href="#"><i class="ion-ios-plus-outline pageAddIcon"></i></a>
+								</div>
+							</li>								
+						</ul>
+					</div>
+				</section>
+			</div>
+		</div>
+	</div>
+
+
+</main>
+</body>
+</html>
+
+
